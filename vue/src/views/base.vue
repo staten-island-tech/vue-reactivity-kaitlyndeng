39fac51369649dd5d6f7f067644ace84733cc03b
kaitlyndeng <kaitlyndeng20@gmail.com>
2023-03-23 10:15:45 -0400

    Commit 11

--- vue/src/views/base.vue
+++ vue/src/views/base.vue
@@ -1,12 +1,42 @@
+<script>
+export default {
+  name: "base",
+  components: {},
+  data() {
+    return {
+      flavors: [
+        {
+          type: "vanilla",
+          vegan: "true",
+          desc: "desc",
+          img: <a ref src="https://www.rachelcooks.com/wp-content/uploads/2022/05/no-churn-vanilla-ice-cream-1500R-13-square.jpg">,
+        },
+        { type: "strawberry", vegan: "true", desc: "desc" },
+        { type: "chocolate", vegan: "false", desc: "desc" },
+        { type: "cookies n' cream", vegan: "false", desc: "desc" },
+        { type: "rum raisin", vegan: "true", desc: "desc" },
+        { type: "rocky road", vegan: "false", desc: "desc" },
+      ],
+    };
+  },
+};
+</script>
 <template>
   <div class="base">
     <h1>Choose all your flavors.</h1>
     <h2>1 flavor = 1 scoop</h2>
+    <div class="flavors"></div>
     <div v-for="flavor in flavors" :key="flavor">
-      {{ flavor.type }}
+      <div class="card">
+        <h3>
+          <b>{{ flavor.type }}</b>
+        </h3>
+        <p>{{ flavor.desc }}</p>
+        {{ flavor.img }}
+      </div>
     </div>
     <!-- <div class="vegan">
-      <h2 v-if="vegan">{{ flavor.type }}</h2>
+      <h2 v-if="vegan === 'true'">{{ flavors.type }}</h2>
       <h2 v-else>Not vegan.</h2>
     </div> -->
 
@@ -24,26 +54,7 @@
     />
     <label for="strawberry">Strawberry</label>
     <br /> -->
+    <div :style="styleObject"></div>
   </div>
 </template>
-
-<script>
-export default {
-  name: "base",
-  components: {},
-  data() {
-    return {
-      flavors: [
-        { type: "vanilla", vegan: "true", desc: "desc" },
-        { type: "strawberry", vegan: "true", desc: "desc" },
-        { type: "chocolate", vegan: "false", desc: "desc" },
-        { type: "cookies n' cream", vegan: "false", desc: "desc" },
-        { type: "rum raisin", vegan: "true", desc: "desc" },
-        { type: "rocky road", vegan: "false", desc: "desc" },
-      ],
-    };
-  },
-};
-</script>
-
 <style></style>
